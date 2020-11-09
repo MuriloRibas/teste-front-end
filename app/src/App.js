90e77aee7e18b5b5f9255f0c2b3f4f8bc73a0797
MuriloRibas <muriloribasss@hotmail.com>
2020-11-09 15:51:52 -0300

    Created react app; Added rmwc lib to use material io web components; Added normalize.css

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@rmwc/button';
+import '@material/button/dist/mdc.button.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Test</Button>
+    </div>
+  );
+}
+
+export default App;
