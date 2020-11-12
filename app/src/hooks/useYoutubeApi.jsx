68c7e6e1f1778629df7b05f9eb2bde85df723072
MuriloRibas <muriloribasss@hotmail.com>
2020-11-12 14:37:17 -0300

    Created loading component; Created infinite pagination.

--- app/src/hooks/useYoutubeApi.jsx
+++ app/src/hooks/useYoutubeApi.jsx
@@ -1,18 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+// LOADING
+// DOIS SCROLLS DIRETOS = DUAS REQUESTS
+
 const useYoutubeApi = () => {
     const [url, setUrl] = useState('https://www.googleapis.com/youtube/v3/search')
     const [key, setKey] = useState(process.env.REACT_APP_API_KEY || '')
 
     const requestSearch = (q) => {
         const qWithoutWithespaces = q.split(' ').join('+')
-        const urlWithQueries = url + '?part=id,snippet&q=' + qWithoutWithespaces + '&key=' + key
+        const urlWithQueries = url + '?part=id,snippet&maxResults=6&q=' + qWithoutWithespaces + '&key=' + key
+        console.log(urlWithQueries)
+        return axios(urlWithQueries)
+    } 
+
+    const requestSearchByPage = (q, token) => {
+        const qWithoutWithespaces = q.split(' ').join('+')
+        const urlWithQueries = url + '?part=id,snippet&maxResults=6&pageToken=' + token + '&q=' + qWithoutWithespaces + '&key=' + key
+        console.log(urlWithQueries)
         return axios(urlWithQueries)
     } 
 
     return {
-        requestSearch
+        requestSearch,
+        requestSearchByPage
     }
 }
 
